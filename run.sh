f27d936de352541a2a8977dc37c6f6957aed59c5
Alexandro <alex@rc.unesp.br>
2024-10-01 13:37:33 -0300

    changing some definitions for easier running of experiments

--- run.sh
+++ run.sh
@@ -22,11 +22,11 @@ if [ -d "$1" ]; then
 fi
 
 # DISABLE THP
-echo never > /sys/kernel/mm/transparent_hugepage/enabled
+###echo never > /sys/kernel/mm/transparent_hugepage/enabled
 # DISABLE NUMA BALANCING
-echo 0 > /proc/sys/kernel/numa_balancing
+###echo 0 > /proc/sys/kernel/numa_balancing
 # DISABLE HW prefetchers?
-bash control_hw_prefetch.sh d
+###bash control_hw_prefetch.sh d
 
 #make clean; make -j12
 echo "Creating new directory:" $PMEM
@@ -52,14 +52,14 @@ eval $CLEANUP; $PREFIX $BENCH_DIR/bench -i immer -d stack -f $PMEM/bench -e 1000
 eval $CLEANUP; $PREFIX $BENCH_DIR/bench -i immer -d vector -f $PMEM/bench -e 1000000 -n 1000000 -t 1 -w 50 -p rand -s120
 eval $CLEANUP; $PREFIX $BENCH_DIR/bench -i immer -d vector-swaps -f $PMEM/bench -e 1000000 -n 1000000 -t 1 -w 100 -p rand -s120
 
-eval $CLEANUP; $PREFIX  $MOD_DIR/build_vacation/vacation/flush/vacation $PMEM/vacation  -r100000 -t200000 -n1 -q55 -u99
-sleep 1
-eval $CLEANUP; $PREFIX $MOD_DIR/build_vacation/memcached-1.2.4/flush/memcached $PMEM/memcached -u root -p 11211 -l 127.0.0.1 &
-sleep 10
-${MEMSLAP_DIR}/memslap -s 127.0.0.1:11211 -c 4 -x 100000 -T 4 -X 512 -F ${MEMCACHED_DIR}/run.cnf -d 1
-pkill memcached
+#eval $CLEANUP; $PREFIX  $MOD_DIR/build_vacation/vacation/flush/vacation $PMEM/vacation  -r100000 -t200000 -n1 -q55 -u99
+#sleep 1
+#eval $CLEANUP; $PREFIX $MOD_DIR/build_vacation/memcached-1.2.4/flush/memcached $PMEM/memcached -u root -p 11211 -l 127.0.0.1 &
+#sleep 10
+#${MEMSLAP_DIR}/memslap -s 127.0.0.1:11211 -c 4 -x 100000 -T 4 -X 512 -F ${MEMCACHED_DIR}/run.cnf -d 1
+#pkill memcached
 
-eval $CLEANUP; $PREFIX $BFS_DIR/mod_bfs $PMEM/bfs $BFS_DIR/flickr.mtx0 
+#eval $CLEANUP; $PREFIX $BFS_DIR/mod_bfs $PMEM/bfs $BFS_DIR/flickr.mtx0 
 } > mod_flush.log 2>> error.log
 
 echo "Running MOD workloads w/o flushing, results in mod_no_flush.log"
@@ -74,15 +74,15 @@ eval $CLEANUP; $PREFIX $BENCH_DIR/bench_no_flush -i immer -d stack -f $PMEM/benc
 
 eval $CLEANUP; $PREFIX $BENCH_DIR/bench_no_flush -i immer -d vector -f $PMEM/bench -e 1000000 -n 1000000 -t 1 -w 50 -p rand -s120
 eval $CLEANUP; $PREFIX $BENCH_DIR/bench_no_flush -i immer -d vector-swaps -f $PMEM/bench -e 1000000 -n 1000000 -t 1 -w 100 -p rand -s120
-sleep 2
-eval $CLEANUP; $PREFIX  $MOD_DIR/build_vacation/vacation/no_flush/vacation_no_flush $PMEM/vacation -r100000 -t200000 -n1 -q55 -u99
-sleep 2
-eval $CLEANUP; $PREFIX $MOD_DIR/build_vacation/memcached-1.2.4/no_flush/memcached_no_flush $PMEM/memcached -u root -p 11211 -l 127.0.0.1 &
-sleep 10
-${MEMSLAP_DIR}/memslap -s 127.0.0.1:11211 -c 4 -x 100000 -T 4 -X 512 -F ${MEMSLAP_DIR}/run.cnf -d 1
-pkill memcached
-sleep 4;
-eval $CLEANUP; $PREFIX $BFS_DIR/mod_bfs_noflush $PMEM/bfs $BFS_DIR/flickr.mtx0 
+#sleep 2
+#eval $CLEANUP; $PREFIX  $MOD_DIR/build_vacation/vacation/no_flush/vacation_no_flush $PMEM/vacation -r100000 -t200000 -n1 -q55 -u99
+#sleep 2
+#eval $CLEANUP; $PREFIX $MOD_DIR/build_vacation/memcached-1.2.4/no_flush/memcached_no_flush $PMEM/memcached -u root -p 11211 -l 127.0.0.1 &
+#sleep 10
+#${MEMSLAP_DIR}/memslap -s 127.0.0.1:11211 -c 4 -x 100000 -T 4 -X 512 -F ${MEMSLAP_DIR}/run.cnf -d 1
+#pkill memcached
+#sleep 4;
+#eval $CLEANUP; $PREFIX $BFS_DIR/mod_bfs_noflush $PMEM/bfs $BFS_DIR/flickr.mtx0 
 } > mod_no_flush.log 2>> error.log
 
 echo "Running PMDK workloads, results in pmdk.log"
@@ -99,13 +99,13 @@ eval $CLEANUP; $PREFIX $BENCH_DIR/bench -i pmdk -d vector -f $PMEM/bench -e 1000
 eval $CLEANUP; $PREFIX $BENCH_DIR/bench -i pmdk -d vector-swaps -f $PMEM/bench -e 1000000 -n 1000000 -t 1 -w 100 -p rand -s120
 
 eval $CLEANUP;  $PREFIX  $VACATION_DIR/build/vacation $PMEM/vacation  -r100000 -t200000 -n1 -q80 -u55;
-sleep 1
-eval $CLEANUP; mkdir $PMEM/memcached; $PREFIX $MEMCACHED_DIR/build/memcached $PMEM/memcached -u root -p 11211 -l 127.0.0.1 &
-sleep 10
-${MEMSLAP_DIR}/memslap -s 127.0.0.1:11211 -c 4 -x 100000 -T 4 -X 512 -F ${MEMSLAP_DIR}/run.cnf -d 1
-pkill memcached
+#sleep 1
+#eval $CLEANUP; mkdir $PMEM/memcached; $PREFIX $MEMCACHED_DIR/build/memcached $PMEM/memcached -u root -p 11211 -l 127.0.0.1 &
+#sleep 10
+#${MEMSLAP_DIR}/memslap -s 127.0.0.1:11211 -c 4 -x 100000 -T 4 -X 512 -F ${MEMSLAP_DIR}/run.cnf -d 1
+#pkill memcached
 
-eval $CLEANUP; $PREFIX $BFS_DIR/pmdk_bfs $PMEM/bfs $BFS_DIR/flickr.mtx0 
+#eval $CLEANUP; $PREFIX $BFS_DIR/pmdk_bfs $PMEM/bfs $BFS_DIR/flickr.mtx0 
 } > pmdk.log 2>> error.log 
 
 echo "Running PMDK workloads w/o flushes, results in pmdk_no_flush.log"
@@ -123,14 +123,14 @@ eval $CLEANUP; $PREFIX $BENCH_DIR/bench -i pmdk -d stack -f $PMEM/bench -e 10000
 eval $CLEANUP; $PREFIX $BENCH_DIR/bench -i pmdk -d vector -f $PMEM/bench -e 1000000 -n 1000000 -t 1 -w 50 -p rand -s120
 eval $CLEANUP; $PREFIX $BENCH_DIR/bench -i pmdk -d vector-swaps -f $PMEM/bench -e 1000000 -n 1000000 -t 1 -w 100 -p rand -s120
 
-eval $CLEANUP; $PREFIX  $VACATION_DIR/build/vacation $PMEM/vacation  -r100000 -t200000 -n1 -q80 -u55;
-sleep 1
-eval $CLEANUP; mkdir $PMEM/memcached; $PREFIX $MEMCACHED_DIR/build/memcached $PMEM/memcached -u root -p 11211 -l 127.0.0.1 &
-sleep 10
-${MEMSLAP_DIR}/memslap -s 127.0.0.1:11211 -c 4 -x 100000 -T 4 -X 512 -F ${MEMSLAP_DIR}/run.cnf -d 1
-pkill memcached
+#eval $CLEANUP; $PREFIX  $VACATION_DIR/build/vacation $PMEM/vacation  -r100000 -t200000 -n1 -q80 -u55;
+#sleep 1
+#eval $CLEANUP; mkdir $PMEM/memcached; $PREFIX $MEMCACHED_DIR/build/memcached $PMEM/memcached -u root -p 11211 -l 127.0.0.1 &
+#sleep 10
+#${MEMSLAP_DIR}/memslap -s 127.0.0.1:11211 -c 4 -x 100000 -T 4 -X 512 -F ${MEMSLAP_DIR}/run.cnf -d 1
+#pkill memcached
 
-eval $CLEANUP; $PREFIX $BFS_DIR/pmdk_bfs $PMEM/bfs $BFS_DIR/flickr.mtx0 
+#eval $CLEANUP; $PREFIX $BFS_DIR/pmdk_bfs $PMEM/bfs $BFS_DIR/flickr.mtx0 
 } > pmdk_no_flush.log 2>> error.log
 
 echo "Running PMDK workloads w/o flushing or logging, results in pmdk_no_log.log"
@@ -150,12 +150,12 @@ eval $CLEANUP; $PREFIX $BENCH_DIR/bench -i pmdk -d stack -f $PMEM/bench -e 10000
 eval $CLEANUP; $PREFIX $BENCH_DIR/bench -i pmdk -d vector -f $PMEM/bench -e 1000000 -n 1000000 -t 1 -w 50 -p rand -s120
 eval $CLEANUP; $PREFIX $BENCH_DIR/bench -i pmdk -d vector-swaps -f $PMEM/bench -e 1000000 -n 1000000 -t 1 -w 100 -p rand -s120
 
-eval $CLEANUP; $PREFIX  $VACATION_DIR/build/vacation_notx $PMEM/vacation  -r100000 -t200000 -n1 -q80 -u55;
-sleep 1
-eval $CLEANUP; mkdir $PMEM/memcached; $PREFIX $MEMCACHED_DIR/build/memcached $PMEM/memcached -u root -p 11211 -l 127.0.0.1 &
-sleep 10
-${MEMSLAP_DIR}/memslap -s 127.0.0.1:11211 -c 4 -x 100000 -T 4 -X 512 -F ${MEMSLAP_DIR}/run.cnf -d 1
-pkill memcached
+#eval $CLEANUP; $PREFIX  $VACATION_DIR/build/vacation_notx $PMEM/vacation  -r100000 -t200000 -n1 -q80 -u55;
+#sleep 1
+#eval $CLEANUP; mkdir $PMEM/memcached; $PREFIX $MEMCACHED_DIR/build/memcached $PMEM/memcached -u root -p 11211 -l 127.0.0.1 &
+#sleep 10
+#${MEMSLAP_DIR}/memslap -s 127.0.0.1:11211 -c 4 -x 100000 -T 4 -X 512 -F ${MEMSLAP_DIR}/run.cnf -d 1
+#pkill memcached
 
-eval $CLEANUP;$PREFIX $BFS_DIR/pmdk_bfs $PMEM/bfs $BFS_DIR/flickr.mtx0 
+#eval $CLEANUP;$PREFIX $BFS_DIR/pmdk_bfs $PMEM/bfs $BFS_DIR/flickr.mtx0 
 } > pmdk_no_log.log 2>> error.log
--- source.sh
+++ source.sh
@@ -1,3 +1,3 @@
 export LD_LIBRARY_PATH=memcached-1.2.4/library/:$LD_LIBRARY_PATH
-export LD_LIBRARY_PATH=pmdk/src/tx-nondebug/:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=pmdk-1.5/src/tx-nondebug/:$LD_LIBRARY_PATH
 export LD_LIBRARY_PATH=nvm_malloc/:$LD_LIBRARY_PATH
